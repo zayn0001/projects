945af6be9ebe6b080fcab35d348c2e32b657a62d
Mishal <mishal0404@gmail.com>
2022-01-11 00:25:41 +0530

    hi

--- learningjavascript/js/script.js
+++ learningjavascript/js/script.js
@@ -1,1 +1,15 @@
-console.log("hi")
+var x = "Hello World";
+
+function a () {}
+
+var a = function () {}
+
+a();
+
+function compare (x,y) {
+	return x>y
+}
+
+var a = compare(4,5);
+compare(4, "a");
+compare();
