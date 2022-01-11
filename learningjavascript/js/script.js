33687fcb2048be227b89fda38af7c7aa2983faa6
Mishal <mishal0404@gmail.com>
2022-01-11 21:15:28 +0530

    yo

--- learningjavascript/js/script.js
+++ learningjavascript/js/script.js
@@ -5,11 +5,16 @@ function a () {}
 var a = function () {}
 
 a();
-
 function compare (x,y) {
 	return x>y
 }
-
 var a = compare(4,5);
 compare(4, "a");
 compare();
+
+var parameter = parameter || "whatever";
+
+if (a == 5) {}
+else {}
+
+for (var i=0;i<10;i++) {}
