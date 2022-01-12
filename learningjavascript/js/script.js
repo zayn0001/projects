0b5da718e7dfaeb6467b115d908ea5dcb77fe152
Mishal <mishal0404@gmail.com>
2022-01-12 19:55:44 +0530

    hi

--- learningjavascript/js/script.js
+++ learningjavascript/js/script.js
@@ -18,3 +18,60 @@ if (a == 5) {}
 else {}
 
 for (var i=0;i<10;i++) {}
+
+var company = new Object();
+company.name = "Facebook";
+company.ceo = new Object();
+company.ceo.firstname = "Mark";
+console.log(company.name);
+console.log(company);
+
+// object literal
+var insta = {
+	name: "sdlkf",
+	ceo: {
+		firstname: "sdlkfj",
+		favcolor: "sdflkj"
+	},
+	"stock of company": 110
+};
+
+function multiply(a,b) {
+	return a*b;
+}
+
+multiply.version = "sdfsdf";
+console.log(multiply.version);
+
+function makemultiplier(multiply) {
+	var myfunc = function(x) {
+		return multiply * x;
+	}
+	return myfunc;
+}
+function dooperation(x, operation){
+	return operation(X);
+};
+var multiplyby3 = makemultiplier(3);
+console.log(multiplyby3(10));
+
+function test() {
+	console.log(this);
+};
+test();
+
+
+function Circle (radius) {
+	this.radius = radius;
+	
+}
+
+Circle.prototype.getarea = function() {
+	return Math.PI * Math.pow(this.radius, 2);
+};
+
+var mycircle = new Circle(10);
+console.log(mycircle);
+
+
+
