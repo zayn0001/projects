982a169dfbf2c8b28f74229dfc22ccfcae63f8f7
Mishal <mishal0404@gmail.com>
2022-01-19 00:07:59 +0530

    json ajax

--- learning javascript/js/script.js
+++ learning javascript/js/script.js
@@ -1,3 +1,4 @@
+/** 
 var x = "Hello World";
 
 function a () {}
@@ -114,7 +115,70 @@ for (var yo in names) {
 	var amanda = 5;
 	window.amanda = amanda;
 })(window);
+**/
+
+document.addEventListener("DOMContentLoaded", function(event){
+	// function sayhello() {
+	// 	var r = document.getElementById("name").value;
+	// 	this.textContent = "said it"
+	// 	console.log(event);
+	// 	var msg = "hello "+ r;
+	// 	var msg2 = "<h2>hello "+ r +"</h2>";
+	// 	document
+	// 		.getElementById("content")
+	// 		.textContent = msg;
+		
+	// 		document
+	// 		.getElementById("content2")
+	// 		.innerHTML = msg2;
+		
+		
+		
+	// 		if (r === "student") {
+	// 			var title = document.querySelector("#title").textContent;
+	// 			title += " lsdfisfj";
+	// 			document.querySelector("#title").textContent = title;
+	// 		}
+	// 	}
+		
+		
+	document.querySelector("button")
+		.addEventListener("click", function(){
+
+			$ajaxUtils
+				.sendGetRequest("data/name.json", 
+				function(res){
+					var msg = res.firstname + " "+ res.lastname;
+					if (res.likeschinesefood) {
+						msg += " likes chinese food"
+					}
+					else {
+						msg += "doesn't like chinese food"
+					}
+					
+
+					document.querySelector("#content")
+					.innerHTML = msg
+				});
+			
+				
+
+		});
+		
+	// document.querySelector("button")
+	// .onclick = sayhello
 
 
+	// document.querySelector("body")
+	// .addEventListener("mousemove", 
+	// function(event) {
+	// 	if (event.shiftKey === true) {
+	// 	console.log(event.clientX)
+	// 	console.log(event.clientY)
+	// 	}
+	// })
+	
 
+
+});
 
