2e0170dadc900371b57a368a607e742c65288d39
Mishal <mishal0404@gmail.com>
2022-01-19 18:49:39 +0530

    uygkhjf

--- Hackathon/Technophilia/js/script.js
+++ Hackathon/Technophilia/js/script.js
@@ -3,31 +3,31 @@ document.querySelector("#eventtype1").onclick = function() {
     document.querySelector("#eventendtime").value = '23:59';
     $("#eventstarttime").prop('readonly', true);
     $("#eventendtime").prop('readonly', true);
-    document.querySelectorAll(".timeset")[0].style.display = '';
-    document.querySelectorAll(".timeset")[1].style.display = '';
+    document.querySelector(".timeset1").style.display = 'block';
+    document.querySelector(".timeset2").style.display = 'block';
 }
 
 
 document.querySelector("#eventtype2").onclick = function() {  
-document.querySelector("#eventstarttime").value = '';
-document.querySelector("#eventendtime").value = '';
-$("#eventstarttime").prop('readonly', false);
-$("#eventendtime").prop('readonly', false);
-document.querySelectorAll(".timeset")[0].style.display = '';
-document.querySelectorAll(".timeset")[1].style.display = '';
+    document.querySelector("#eventstarttime").value = '';
+    document.querySelector("#eventendtime").value = '';
+    $("#eventstarttime").prop('readonly', false);
+    $("#eventendtime").prop('readonly', false);
+    document.querySelector(".timeset1").style.display = 'block';
+    document.querySelector(".timeset2").style.display = 'block';
 }
 
 document.querySelector("#myform").addEventListener("submit",
-function(event){
-    console.log("here")
-    event.preventDefault();
-    handleForm(this)
+    function(event){
+        console.log("here")
+        event.preventDefault();
+        handleForm(this)
 }, true)
 
 function handleForm(form){
 
     var missing = false
-    var compulsory = ['eventname','eventimp','eventdate','eventype','eventstarttime','eventendtime','desc','recurrance','eventpurpose']
+    var compulsory = ['eventname','eventimp','eventdate','eventtype','eventstarttime','eventendtime','desc','recurrance','eventpurpose']
     for (i=0;i<compulsory.length;i++) {
         if (form[compulsory[i]].value == '') {
             console.log("missing parameters but defined!!" + compulsory[i]);
@@ -35,6 +35,7 @@ function handleForm(form){
             console.log(missing)
         }
     }
+
     const d = new Date();
     currentdate = d.getDate();
     currentmonth = d.getMonth();
@@ -44,23 +45,34 @@ function handleForm(form){
     inputmonth = form['eventdate'].value.split("-")[1];
     inputyear = form['eventdate'].value.split("-")[0];
     currenthour = d.getHours();
-    inputhour = form['eventstarttime'].value.split(":")[0]
+    currentmin = d.getMinutes();
+    inputstarthour = form['eventstarttime'].value.split(":")[0]
+    inputendhour = form['eventendtime'].value.split(":")[0]
+    inputstartmin = form['eventstarttime'].value.split(":")[1]
+    inputendmin = form['eventendtime'].value.split(":")[1]
+    eventtype = form["eventtype"].value;
+
     if (missing==true) {
         console.log("reached here")
         console.log(currenthour);
-        console.log(inputhour);
-        document.querySelector(".alertmsg").style.display = ""
+        console.log(inputstarthour);
+        document.querySelector(".alertmsg").style.display = "block"
         document.querySelector(".alertmsg").innerHTML = "Please fill all necessary inputs before submitting"
-
     }
     else if (inputyear < currentyear || (currentyear==inputyear && inputmonth < currentmonth) || (currentyear==inputyear && inputmonth == currentmonth && inputdate < currentdate)) {
         console.log("invalid date")
-        document.querySelector(".alertmsg").style.display = ""
+        document.querySelector(".alertmsg").style.display = "block"
         document.querySelector(".alertmsg").innerHTML = "Invalid Date"
     }
-    else if (currentyear==inputyear && inputmonth == currentmonth && inputdate == currentdate && inputhour <= currenthour) {
-        document.querySelector(".alertmsg").style.display = ""
-        document.querySelector(".alertmsg").innerHTML = "Please select a Start Time atleast one hour from now"
+    else if (currentyear==inputyear && inputmonth == currentmonth && inputdate == currentdate && (inputstarthour < currenthour ||  (inputstarthour==currenthour && inputstartmin < (currentmin + 10))) && eventtype == "Time Based") {
+        document.querySelector(".alertmsg").style.display = "block"
+        document.querySelector(".alertmsg").innerHTML = "Please select a Start Time atleast 10 minutes from now"
+        console.log("here now")
+    }
+    else if (inputendhour < inputstarthour || (inputendhour == inputstarthour && inputendmin <= inputstartmin)) {
+        document.querySelector(".alertmsg").style.display = "block"
+        document.querySelector(".alertmsg").innerHTML = "Invalid End Time"
+        console.log("here now 2")
     }
     else {
         console.log("SUccesss!!!")
