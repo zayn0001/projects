71d11d271552bc37babb89407f4b1ed8756d8470
Mishal <mishal0404@gmail.com>
2022-01-21 16:12:33 +0530

    jhjk

--- Hackathon/Technophilia/js/script.js
+++ Hackathon/Technophilia/js/script.js
@@ -19,10 +19,19 @@ document.querySelector("#eventtype2").onclick = function() {
 
 document.querySelector("#myform").addEventListener("submit",
     function(event){
-        console.log("here")
+        //console.log("here")
         event.preventDefault();
         handleForm(this)
 }, true)
+
+function disableButton(button) {
+    //console.log("tporuwpeoriwep")
+    document.querySelector(button).disabled = true;
+    //console.log("yooo")
+    document.querySelector(button).style["background-color"] = "#D3D3D3";
+    //console.log("pwoli")
+    document.querySelector(button).style["color"] = "#EBECF0";
+}
 
 function handleForm(form){
 
@@ -30,13 +39,13 @@ function handleForm(form){
     var compulsory = ['eventname','eventimp','eventdate','eventtype','eventstarttime','eventendtime','desc','recurrance','eventpurpose']
     for (i=0;i<compulsory.length;i++) {
         if (form[compulsory[i]].value == '') {
-            console.log("missing parameters but defined!!" + compulsory[i]);
+            //console.log("missing parameters but defined!!" + compulsory[i]);
             missing = true
-            console.log(missing)
+            //console.log(missing)
         }
     }
 
-    const d = new Date();
+    var d = new Date();
     currentdate = d.getDate();
     currentmonth = d.getMonth();
     currentmonth = currentmonth + 1;
@@ -53,30 +62,31 @@ function handleForm(form){
     eventtype = form["eventtype"].value;
 
     if (missing==true) {
-        console.log("reached here")
-        console.log(currenthour);
-        console.log(inputstarthour);
+        //console.log("reached here")
+        ////console.log(currenthour);
+        ////console.log(inputstarthour);
         document.querySelector(".alertmsg").style.display = "block"
         document.querySelector(".alertmsg").innerHTML = "Please fill all necessary inputs before submitting"
     }
     else if (inputyear < currentyear || (currentyear==inputyear && inputmonth < currentmonth) || (currentyear==inputyear && inputmonth == currentmonth && inputdate < currentdate)) {
-        console.log("invalid date")
+        //console.log("invalid date")
         document.querySelector(".alertmsg").style.display = "block"
         document.querySelector(".alertmsg").innerHTML = "Invalid Date"
     }
     else if (currentyear==inputyear && inputmonth == currentmonth && inputdate == currentdate && (inputstarthour < currenthour ||  (inputstarthour==currenthour && inputstartmin < (currentmin + 10))) && eventtype == "Time Based") {
         document.querySelector(".alertmsg").style.display = "block"
         document.querySelector(".alertmsg").innerHTML = "Please select a Start Time atleast 10 minutes from now"
-        console.log("here now")
+        ////console.log("here now")
     }
     else if (inputendhour < inputstarthour || (inputendhour == inputstarthour && inputendmin <= inputstartmin)) {
         document.querySelector(".alertmsg").style.display = "block"
         document.querySelector(".alertmsg").innerHTML = "Invalid End Time"
-        console.log("here now 2")
+        ////console.log("here now 2")
     }
     else {
-        console.log("SUccesss!!!")
-        google.script.run.hello(form);
-        location.replace("https://www.google.com/")
+        //console.log("SUccesss!!!")
+        google.script.run.createEvent(form);
+        //location.replace("https://www.google.com")
+        disableButton("#submit");
     }
 }
